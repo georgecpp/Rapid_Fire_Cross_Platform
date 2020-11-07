ea8c2111363bff7b8dc2aa70d732e0a72df8754b
georgecpp <73043384+georgecpp@users.noreply.github.com>
2020-11-07 23:35:34 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty, NumericProperty, ListProperty, ReferenceListProperty, BooleanProperty
-from kivy.core.window import Window
-from kivy.config import Config
 from kivy.clock import Clock
 from kivy.vector import Vector
-from kivy.uix.floatlayout import FloatLayout
 from random import randint
-import random, time
+import random
+from kivy.core.audio import SoundLoader
 
 Builder.load_file("RapidFireApp.kv")
+sound = SoundLoader.load('crazytrain.mp3')
 
 
 # Declare both screens
@@ -23,7 +22,9 @@ class PlayScreen(Screen):
         super(PlayScreen, self).__init__(**kwargs)
 
     def on_enter(self, *args):
-        Clock.schedule_interval(self.rapid_fire_game.appear_target, 1 / 1)
+        self.rapid_fire_game.eventTick = Clock.schedule_interval(self.rapid_fire_game.update_timer, 1)
+        self.rapid_fire_game.event = Clock.schedule_interval(self.rapid_fire_game.appear_target,
+                                                             1 / self.rapid_fire_game.spawn_time)
 
 
 class MenuScreen(Screen):
@@ -38,7 +39,10 @@ class Target(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
+    deltaPos_x = NumericProperty(0.0)
+    deltaPos_y = NumericProperty(0.0)
     shoot = BooleanProperty(False)
+    score_range = NumericProperty(0)
 
     def __init__(self, **kwargs):
         super(Target, self).__init__(**kwargs)
@@ -46,13 +50,34 @@ class Target(Widget):
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
             self.shoot = True
-
+            self.pressed = touch.pos
+            self.deltaPos_x = touch.pos[0] - self.pos[0]
+            self.deltaPos_y = touch.pos[1] - self.pos[1]
+            # CHECK FOR SCORE! CONCENTRIC CIRCLES
+            # CASE 1 MAX CENTER - 20
+            # if 48<self.deltaPos_x<51 and
+            if (self.deltaPos_x > 0 and self.deltaPos_x < self.size[0]) and (
+                    self.deltaPos_y > 0 and self.deltaPos_y < self.size[1]):
+                self.score_range = 14
+            if (self.deltaPos_x >= self.size[0] / 5 and self.deltaPos_x <= 0.8 * self.size[0]) and (
+                    self.deltaPos_y >= self.size[1] / 5 and self.deltaPos_y <= 0.8 * self.size[1]):
+                self.score_range = 16
+            if (self.deltaPos_x >= 0.4 * self.size[0] and self.deltaPos_x <= 0.6 * self.size[0]) and (
+                    self.deltaPos_y >= 0.4 * self.size[1] and self.deltaPos_y <= 0.6 * self.size[1]):
+                self.score_range = 18
+            if (self.deltaPos_x >= self.size[0] / 2 - self.size[0] / 50 and self.deltaPos_x <= self.size[0] / 2 +
+                self.size[0] / 50) and (
+                    self.deltaPos_y >= self.size[1] / 2 - self.size[1] / 50 and self.deltaPos_y <= self.size[1] / 2 +
+                    self.size[1] / 50):
+                self.score_range = 20
             # UPDATE ALL : SCORE, LEVEL, SCORE TO LEVEL, SPEED OF TARGETS, SPAWN TIME!!!!!
-            self.parent.score += 10
+            self.parent.score += self.score_range
             if self.parent.score_to_lvl_up > 0:
-                self.parent.score_to_lvl_up -= 10
-                if self.parent.score_to_lvl_up == 0:
-                    self.parent.curr_level += 1
+                self.parent.score_to_lvl_up -= self.score_range
+                if self.parent.score_to_lvl_up <= 0:
+                    self.parent.update_new_level()
+                    # UPDATE ALL - SCORE_LVL UP +=100, SPEED_TARGETS, SPAWN TIME!!!!! - CLOCK BACK TO 60.
+
             self.parent.remove_widget(self)
 
     # Latest position = Current velocity + Current position
@@ -74,9 +99,16 @@ class RapidFireGame(Widget):
     curr_level = NumericProperty(1)
     score = NumericProperty(0)
     score_to_lvl_up = NumericProperty(300)
+    copy_score_lvl_up = NumericProperty(0)
+    spawn_time = NumericProperty(1.5)
+    speed_target = NumericProperty(1.5)
+    timer_val = NumericProperty(30)
+    event = 0
+    eventTick = 0
 
     def __init__(self, **kwargs):
         super(RapidFireGame, self).__init__(**kwargs)
+        self.copy_score_lvl_up = self.score_to_lvl_up
 
     def appear_target(self, *args):
         random_pos = Vector(random.randint(0, self.width), random.uniform(self.main_frame.height / 4,
@@ -84,11 +116,38 @@ class RapidFireGame(Widget):
         target = Target()
         target.pos = random_pos
         if target.pos[1] < self.main_frame.height / 2:
-            target.velocity = Vector(0, 1.5).rotate(randint(0, 360))  # different velocity for different level!!
+            target.velocity = Vector(0, self.speed_target).rotate(
+                randint(0, 360))  # different velocity for different level!!
         if target.pos[1] > self.main_frame.height / 2:
-            target.velocity = Vector(0, -1.5).rotate(randint(0, 360))
+            target.velocity = Vector(0, -1.0 * self.speed_target).rotate(randint(0, 360))
         Clock.schedule_interval(target.update, 1.0 / 60.0)
         self.add_widget(target)
+
+    def update_new_level(self):
+        # self.level_up.opacity = 1
+
+        # for timer
+        self.eventTick.cancel()
+        self.timer_val = 30
+        self.eventTick = Clock.schedule_interval(self.update_timer, 1)
+
+        self.curr_level += 1
+        self.score_to_lvl_up = self.copy_score_lvl_up + 100
+        self.copy_score_lvl_up += 100
+        self.speed_target += 1.0
+
+        # for spawn time modified of targets.
+        self.event.cancel()
+        self.spawn_time += 1.5
+        self.event = Clock.schedule_interval(self.appear_target, 1 / self.spawn_time)
+
+    def update_timer(self, *args):
+        if self.timer_val == 0:  # verify score to level up. if > 0, you lost.
+            if self.score_to_lvl_up >= 0:
+                self.event.cancel()
+                self.eventTick.cancel()
+                return
+        self.timer_val -= 1
 
 
 sm = ScreenManager()
@@ -100,6 +159,8 @@ sm.add_widget(SettingsScreen(name='settings'))
 class RapidFireApp(App):
 
     def build(self):
+        if sound:
+            sound.play()
         return sm
 
 
